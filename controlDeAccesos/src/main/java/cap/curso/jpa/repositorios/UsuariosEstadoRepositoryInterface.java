4adc5363340e9999e32e53452a98641aa5e682c7
mdeanose <mdeanose@LES005127.corp.capgemini.com>
2020-09-21 18:14:50 +0200

    Añadida etiqueta repository

--- controlDeAccesos/src/main/java/cap/curso/jpa/repositorios/UsuariosEstadoRepositoryInterface.java
+++ controlDeAccesos/src/main/java/cap/curso/jpa/repositorios/UsuariosEstadoRepositoryInterface.java
@@ -2,11 +2,11 @@ package cap.curso.jpa.repositorios;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 
 import cap.curso.jpa.entidades.Usuario_Estado;
 
-
+@Repository
 public interface UsuariosEstadoRepositoryInterface extends CrudRepository<Usuario_Estado, Integer>
 {
 
