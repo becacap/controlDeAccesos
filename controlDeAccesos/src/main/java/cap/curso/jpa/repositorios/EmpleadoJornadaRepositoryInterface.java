46486dd9151c5ab609f5ac79eabe7c6627f736d1
Zoe <becacapsept2020@gmail.com>
2020-09-21 17:51:36 +0200

    añadida interfaz repositorio empleados_jornada

--- controlDeAccesos/src/main/java/cap/curso/jpa/repositorios/EmpleadoJornadaRepositoryInterface.java
+++ controlDeAccesos/src/main/java/cap/curso/jpa/repositorios/EmpleadoJornadaRepositoryInterface.java
@@ -1,6 +1,9 @@
 package cap.curso.jpa.repositorios;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import cap.curso.jpa.entidades.Empleado;
@@ -9,5 +12,5 @@ import cap.curso.jpa.entidades.Empleado;
 public interface EmpleadoJornadaRepositoryInterface extends CrudRepository<Empleado, Integer>
 {
 
-	public Empleado modificarJornada(Empleado empleado);
+
 }
