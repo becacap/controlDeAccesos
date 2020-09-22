dcc065a299af49634562fee459c5f22ca0e6962a
rmanriqu <rmanriqu@LES009521.corp.capgemini.com>
2020-09-22 11:11:00 +0200

    Cositas del repositorio

--- controlDeAccesos/src/main/java/cap/curso/jpa/repositorios/UsuariosEstadoRepositoryInterface.java
+++ controlDeAccesos/src/main/java/cap/curso/jpa/repositorios/UsuariosEstadoRepositoryInterface.java
@@ -4,12 +4,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import cap.curso.jpa.entidades.Calendario;
+import cap.curso.jpa.entidades.Empleado;
 import cap.curso.jpa.entidades.Usuario_Estado;
 
 @Repository
 public interface UsuariosEstadoRepositoryInterface extends CrudRepository<Usuario_Estado, Integer>
 {
+	
+	//from Usuario_Estado ue where ue.emplead=:empleado and ue.calendario=:calendario
 
 	@Query("from ((Usuario_Estado r natural join Estado e) natural join Jornada) where r.empleados_id=:empleado_id and e.descripcion='laborable'")
-	public Usuario_Estado getUsuarioEstadoByEmpleado(Integer empleado_id, Integer month, Integer year);
+	public Usuario_Estado getUsuarioEstadoByEmpleado(Empleado empleado,Calendario calendario);
 }
--- controlDeAccesos/src/test/java/cap/curso/jpa/TestFichajesR.java
+++ controlDeAccesos/src/test/java/cap/curso/jpa/TestFichajesR.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cap.curso.jpa.configuracion.Configuracion;
+import cap.curso.jpa.entidades.Usuario_Estado;
 import cap.curso.jpa.services.UsuariosEstadoServiceInterface;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -16,21 +17,35 @@ import cap.curso.jpa.services.UsuariosEstadoServiceInterface;
 public class TestFichajesR
 {
 
-	@Autowired
-	private UsuariosEstadoServiceInterface usuariosEstadoService;
+//	@Autowired
+//	private UsuariosEstadoServiceInterface usuariosEstadoService;
 	
 	@Test
-	public void test()
+	public void testWrongMes ()
 	{
-		fail("Not yet implemented");
+		int idEmpleado 	= 1; 	// Id que existe
+		int year 		= 2018; // Year correcto
+		int mes 		= 15; 	// Mes incorrecto
+		
+//		Usuario_Estado ue = getUsuariosEstadoService().getDiasTrabajadosEmpleadoByYearByMes (idEmpleado, year, mes);
+	}
+	
+//	@Test
+	public void testWrongUsuario ()
+	{
+		int idEmpleado 	= 5590; // Id que no existe
+		int year 		= 2019; // Year correcto
+		int mes 		= 6; 	// Mes correcto
+
+//		¿ assertThrown ? ( getUsuariosEstadoService().getDiasTrabajadosEmpleadoByYearByMes (idEmpleado, year, mes) );
 	}
 
-	public UsuariosEstadoServiceInterface getUsuariosEstadoService() {
-		return usuariosEstadoService;
-	}
-
-	public void setUsuariosEstadoService(UsuariosEstadoServiceInterface usuariosEstadoService) {
-		this.usuariosEstadoService = usuariosEstadoService;
-	}
+//	public UsuariosEstadoServiceInterface getUsuariosEstadoService() {
+//		return usuariosEstadoService;
+//	}
+//
+//	public void setUsuariosEstadoService(UsuariosEstadoServiceInterface usuariosEstadoService) {
+//		this.usuariosEstadoService = usuariosEstadoService;
+//	}
 
 }
